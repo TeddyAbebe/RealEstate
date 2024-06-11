cb2b9a96c26e47ab4feb64e4eb1e961c20de490d
Tewodros Abebe <108568145+TeddyAbebe@users.noreply.github.com>
2024-06-11 10:33:29 +0300

    Complete Profile Page UI

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,6 +1,54 @@
+import { useSelector } from "react-redux";
+import { selectUser } from "../../redux/user/selectors";
 
 export default function Profile() {
+  const { currentUser } = useSelector(selectUser);
+
   return (
-    <div>Profile</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="Profile"
+          className="rounded-full w-24 h-24 object-cover cursor-pointer self-center mt-2"
+        />
+
+        <input
+          type="text"
+          id="username"
+          placeholder="User Name"
+          className="border p-3 rounded-lg"
+        />
+
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+        />
+
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+        />
+
+        <button
+          // disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {/* {loading ? "Loading..." : "Update"} */}
+          Update
+        </button>
+      </form>
+
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 }
